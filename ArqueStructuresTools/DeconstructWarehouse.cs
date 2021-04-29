b37475e4b96206833e59419ec41650143518f133
Christian Dimitri <39223566+christiandimitri@users.noreply.github.com>
2021-04-29 17:00:00 +0200

    Deconstruct warehouse component created

---
+++ ArqueStructuresTools/DeconstructWarehouse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+
+namespace ArqueStructuresTools
+{
+    public class DeconstructWarehouse : GH_Component
+    {
+        public DeconstructWarehouse() : base("DeconstructWarehouse", "Nickname", "Description", "Arque Structures",
+            "Utilities")
+        {
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("45D2DC47-A714-4DFD-A19A-B281CFF1F870"); }
+        }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new WarehouseParameter());
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddParameter(new TrussParameter());
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            WarehouseGoo warehouseGoo = new WarehouseGoo();
+
+            if (!DA.GetData(0, ref warehouseGoo)) return;
+
+            var warehouse = warehouseGoo.Value;
+
+            var trusses = new List<TrussGoo>();
+            foreach (var truss in warehouse.Trusses)
+            {
+                trusses.Add(new TrussGoo(truss));
+            }
+            DA.SetDataList(0, new List<TrussGoo>(trusses));
+        }
+    }
+}
